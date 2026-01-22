0c63714634da8ae448274e63630a80dd833a8b05
Sasha Roth <asr195@pitt.edu>
2026-01-22 16:31:29 -0500

    Added previous 0302 assignment folders to personal repository

---
+++ project1_starter/bitarray.cpp
@@ -0,0 +1,123 @@
+#include "bitarray.hpp"
+
+BitArray::BitArray()
+{
+    valid = true;     // default to valid
+    current_size = 8; // default size
+    data = new uint8_t[current_size];
+    for (int i = 0; i < current_size; i++)
+    {
+        data[i] = 0;
+    }
+}
+
+BitArray::BitArray(intmax_t size)
+{
+    valid = true;        // default to valid
+    current_size = size; // set size based on input
+    if (size <= 0)
+    {
+        data = new uint8_t[0];
+        valid = false;
+    }
+    else
+    {
+        data = new uint8_t[current_size];
+        for (int i = 0; i < current_size; i++)
+        {
+            data[i] = 0;
+        }
+    }
+}
+
+BitArray::BitArray(const std::string &value)
+{
+    valid = true;                // default to valid
+    current_size = value.size(); // set size based on string length
+    data = new uint8_t[current_size];
+    for (int i = 0; i < current_size; i++)
+    {
+        if (value.at(i) != '0' && value.at(i) != '1')
+        {
+            valid = false;
+        }
+        data[i] = value.at(i) - '0';
+    }
+}
+
+// Was having memory deallocation errors, so I referenced stack exchange
+// for proper destructor syntax
+BitArray::~BitArray()
+{
+    delete[] data; // deallocate memory
+}
+
+intmax_t BitArray::size() const
+{
+    return current_size; // return current size
+}
+
+bool BitArray::good() const
+{
+    return valid; // return validity
+}
+
+void BitArray::set(intmax_t index)
+{
+    if (index < 0 || index >= current_size) // bounds checking
+    {
+        valid = false; // mark as invalid if out of bounds
+    }
+    else
+    {
+        data[index] = 1; // set bit to 1
+    }
+}
+
+void BitArray::reset(intmax_t index)
+{
+    if (index < 0 || index >= current_size) // bounds checking
+    {
+        valid = false; // mark as invalid if out of bounds
+    }
+    else
+    {
+        data[index] = 0; // set bit to 0
+    }
+}
+
+void BitArray::toggle(intmax_t index)
+{
+    if (index < 0 || index >= current_size) // bounds checking
+    {
+        valid = false; // mark as invalid if out of bounds
+    }
+    else if (data[index] == 0) // toggle bit
+    {
+        data[index] = 1;
+    }
+    else
+    {
+        data[index] = 0;
+    }
+}
+
+bool BitArray::test(intmax_t index)
+{
+    if (index < 0 || index >= current_size) // bounds checking
+    {
+        valid = false; // mark as invalid if out of bounds
+        return false;
+    }
+    return data[index] == 1; // compare bit to 1
+}
+
+std::string BitArray::asString() const
+{
+    std::string result = ""; // initialize empty string
+    for (int i = 0; i < current_size; i++)
+    {
+        result += std::to_string(data[i]); // append each bit to string
+    }
+    return result; // return the string representation
+}
---
+++ project1_starter/bitarray.hpp
@@ -0,0 +1,53 @@
+#ifndef BITARRAY_HPP
+#define BITARRAY_HPP
+#include <stdint.h>
+#include <string>
+
+#include "bitset.hpp"
+
+class BitArray : public Bitset
+{
+public:
+  /**
+   * Class constructor.
+   */
+  BitArray();
+
+  /**
+   * Class constructor specifying size of array.
+   * @param size Size of the bit array.
+   */
+  BitArray(intmax_t size);
+
+  /**
+   * Class constructor which uses a string to initialize the bit array.
+   * @param value String representing the bit array.
+   */
+  BitArray(const std::string &value);
+
+  ~BitArray();
+
+  BitArray(const BitArray &) = delete;            // not Copy-Constructible
+  BitArray &operator=(const BitArray &) = delete; // not Copy-Assignable
+
+  intmax_t size() const;
+
+  bool good() const;
+
+  void set(intmax_t index);
+
+  void reset(intmax_t index);
+
+  void toggle(intmax_t index);
+
+  bool test(intmax_t index);
+
+  std::string asString() const;
+
+private:
+  intmax_t current_size;
+  uint8_t *data;
+  bool valid;
+};
+
+#endif
