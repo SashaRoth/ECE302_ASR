98d5f6cd37a33d25a602b0ab59eeecc82def607a
Sasha Roth <asr195@pitt.edu>
2026-03-05 20:13:10 -0500

    implemented my own test cases and debugged

--- problem6/test_sorted_list.cpp
+++ problem6/test_sorted_list.cpp
@@ -154,3 +154,189 @@ TEST_CASE("SortedList: Testing list and sorted list std::out_of_range exceptions
 }
 
 /* Your tests here */
+
+//only tests for methods that are modified in sorted_list; other methods that are inherited from List<T> have already been tested in previous assignments
+
+//test constructor from unsorted list
+TEST_CASE("SortedList: Testing constructor taking unsorted list as argument", "[sorted list]")
+{
+  List<float> unsorted;
+
+  unsorted.insert(1, 2.35);
+  unsorted.insert(2, 4.58);
+  unsorted.insert(3, 1.05);
+  unsorted.insert(4, 1.04);
+  unsorted.insert(5, 3.14);
+
+  SortedList<float> sorted(unsorted);
+
+  REQUIRE(sorted.getPosition(1.04) == 1);
+  REQUIRE(sorted.getPosition(1.05) == 2);
+  REQUIRE(sorted.getPosition(2.35) == 3);
+  REQUIRE(sorted.getPosition(3.14) == 4);
+  REQUIRE(sorted.getPosition(4.58) == 5);
+
+  //test exceptions and false returns
+
+  REQUIRE_THROWS_AS(sorted.getPosition(5.0), std::invalid_argument);
+  REQUIRE_FALSE(sorted.getPosition(1.04) == 2);
+}
+
+//test insert
+TEST_CASE("SortedList: Testing insert with various cases", "[sorted list]")
+{
+  SortedList<int> lst;
+
+  //test on empty list
+  lst.insert(5);
+  REQUIRE(lst.getEntry(1) == 5);
+
+  lst.insert(3);
+  REQUIRE(lst.getEntry(1) == 3);
+  REQUIRE(lst.getEntry(2) == 5);
+
+  //test adding very big value
+  lst.insert(100);
+  REQUIRE(lst.getEntry(3) == 100);
+
+  //test adding very small value
+  lst.insert(-10);
+  REQUIRE(lst.getEntry(1) == -10);
+
+  //test adding value in the middle
+  lst.insert(4);
+  REQUIRE(lst.getEntry(3) == 4);
+
+  //make sure array is sorted after multiple insertions
+  REQUIRE(lst.getEntry(1) == -10);
+  REQUIRE(lst.getEntry(2) == 3);
+  REQUIRE(lst.getEntry(3) == 4);
+  REQUIRE(lst.getEntry(4) == 5);
+  REQUIRE(lst.getEntry(5) == 100);
+}
+
+//test remove
+TEST_CASE("SortedList: Testing remove with various cases", "[sorted list]")
+{
+  SortedList<int> lst;
+
+  lst.insert(5);
+  lst.insert(13);
+  lst.insert(2);
+  lst.insert(9);
+  lst.insert(10);
+
+  REQUIRE(lst.getLength() == 5);
+  REQUIRE(lst.getEntry(1) == 2);
+  REQUIRE(lst.getEntry(2) == 5);
+  REQUIRE(lst.getEntry(3) == 9);
+  REQUIRE(lst.getEntry(4) == 10);
+  REQUIRE(lst.getEntry(5) == 13);
+
+  //test removing first element
+  lst.remove(2);
+  REQUIRE(lst.getEntry(1) == 5);
+  REQUIRE(lst.getEntry(2) == 9);
+  REQUIRE(lst.getEntry(3) == 10);
+  REQUIRE(lst.getEntry(4) == 13);
+  lst.insert(2); // add it back for further testing
+  
+
+  //test removing last element
+  lst.remove(13);
+  REQUIRE(lst.getEntry(1) == 2);
+  REQUIRE(lst.getEntry(2) == 5);
+  REQUIRE(lst.getEntry(3) == 9);
+  REQUIRE(lst.getEntry(4) == 10);
+  REQUIRE_THROWS_AS(lst.getEntry(5), std::out_of_range); // make sure 5th element is removed
+  lst.insert(13); // add it back for further testing
+
+  //test removing middle element
+  lst.remove(9);
+  REQUIRE(lst.getEntry(1) == 2);
+  REQUIRE(lst.getEntry(2) == 5);
+  REQUIRE(lst.getEntry(3) == 10);
+  REQUIRE(lst.getEntry(4) == 13);
+
+  //test removing element twice
+  REQUIRE_THROWS_AS(lst.remove(9), std::invalid_argument);
+
+  //test removing non-existent element
+  REQUIRE_THROWS_AS(lst.remove(100), std::invalid_argument);
+}
+
+//test removeAt (essially the same as List<T>::remove, but we can still test it to make sure it works for SortedList)
+TEST_CASE("SortedList: Testing removeAt with various cases", "[sorted list]")
+{
+  SortedList<int> lst;
+
+  lst.insert(5);
+  lst.insert(13);
+  lst.insert(2);
+  lst.insert(9);
+  lst.insert(10);
+
+  REQUIRE(lst.getLength() == 5);
+  REQUIRE(lst.getEntry(1) == 2);
+  REQUIRE(lst.getEntry(2) == 5);
+  REQUIRE(lst.getEntry(3) == 9);
+  REQUIRE(lst.getEntry(4) == 10);
+  REQUIRE(lst.getEntry(5) == 13);
+
+  //test removing first element
+  lst.removeAt(1);
+  REQUIRE(lst.getEntry(1) == 5);
+  REQUIRE(lst.getEntry(2) == 9);
+  REQUIRE(lst.getEntry(3) == 10);
+  REQUIRE(lst.getEntry(4) == 13);
+  
+  //test removing last element
+  lst.removeAt(4);
+  REQUIRE(lst.getEntry(1) == 5);
+  REQUIRE(lst.getEntry(2) == 9);
+  REQUIRE(lst.getEntry(3) == 10);
+  
+  //test removing middle element
+  lst.removeAt(2);
+  REQUIRE(lst.getEntry(1) == 5);
+  REQUIRE(lst.getEntry(2) == 10);
+
+   //test removing element at invalid position
+   REQUIRE_THROWS_AS(lst.removeAt(3), std::out_of_range); // only two elements left, so position 3 is invalid
+   REQUIRE_THROWS_AS(lst.removeAt(-1), std::out_of_range); // negative position is invalid
+}
+
+//test getEntry
+TEST_CASE("SortedList: Testing getEntry with various cases", "[sorted list]")
+{
+  SortedList<std::string> lst;
+
+  lst.insert("robot");
+  lst.insert("smoothie");
+  lst.insert("spring break");
+
+  REQUIRE(lst.getEntry(1) == "robot");
+  REQUIRE(lst.getEntry(2) == "smoothie");
+  REQUIRE(lst.getEntry(3) == "spring break");
+
+  //test getEntry with invalid position
+  REQUIRE_THROWS_AS(lst.getEntry(0), std::out_of_range); // 1-indexed, 0 is invalid
+  REQUIRE_THROWS_AS(lst.getEntry(4), std::out_of_range); // out of range
+}
+
+//test getPosition
+TEST_CASE("SortedList: Testing getPosition with various cases", "[sorted list]")
+{
+  SortedList<std::string> lst;
+
+  lst.insert("celsius energy drink");
+  lst.insert("hi Gavin if you're reading this");
+  lst.insert("I am going to sleep so much next week");
+
+  REQUIRE(lst.getPosition("I am going to sleep so much next week") == 1); //sorts uppercase first according to ascii code, even though "i" is last alphabetically
+  REQUIRE(lst.getPosition("celsius energy drink") == 2);
+  REQUIRE(lst.getPosition("hi Gavin if you're reading this") == 3);
+
+  //test getPosition with non-existent item: "my sleep schedule" is nonexistent
+  REQUIRE_THROWS_AS(lst.getPosition("my sleep schedule"), std::invalid_argument);
+}
